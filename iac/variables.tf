71fb157c967c0fff16fadb230f40adbf7808a72d
Shraddha Aangiras <shraddhaaangiras0911@gmail.com>
2025-07-01 15:46:38 +0530

    Added backend view counter

--- iac/variables.tf
+++ iac/variables.tf
@@ -15,3 +15,17 @@ variable "domain_name" {
   description = "The custom domain name to be used for SSL certificate"
   type        = string
 }
+
+variable "uptime_check_name" {
+  default = "visitor-api-uptime"
+}
+
+variable "uptime_check_url" {
+  description = "The API Gateway URL with /health-check endpoint"
+  type        = string
+}
+
+variable "notification_email" {
+  type = string
+}
+
